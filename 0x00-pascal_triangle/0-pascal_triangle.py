6db0ca6b3218e11976fd5a216e06e9b520f30cb3
Y4h14 <yahyasadig2013@gmail.com>
2024-06-03 01:24:43 +0300

    fixed styling

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
+"""defines a pascal triangle function"""
+
+
 def pascal_triangle(n):
     """
+    returns the nth row in the pascal triagle
     """
-    if n <= 0 :
+    if n <= 0:
         return []
-    
     if n == 1:
         return [[1]]
-    else: 
+    else:
         result = pascal_triangle(n - 1)
     row = [1]
-    prev= result[-1]
+    prev = result[-1]
     for i in range(len(prev) - 1):
         row.append(prev[1] + prev[i+1])
     row += [1]
     result.append(row)
     return result
-        
