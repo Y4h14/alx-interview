7d18028f048573c72ad24f361ec500c3e42bd475
Y4h14 <yahyasadig2013@gmail.com>
2024-06-03 01:21:39 +0300

    first attempt

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+def pascal_triangle(n):
+    """
+    """
+    if n <= 0 :
+        return []
+    
+    if n == 1:
+        return [[1]]
+    else: 
+        result = pascal_triangle(n - 1)
+    row = [1]
+    prev= result[-1]
+    for i in range(len(prev) - 1):
+        row.append(prev[1] + prev[i+1])
+    row += [1]
+    result.append(row)
+    return result
+        
