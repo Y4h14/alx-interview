25ab8f7b8dc2ed5fde69dc09a16c1b864a12082b
Y4h14 <yahyasadig2013@gmail.com>
2024-07-13 16:35:35 +0300

    fixed code styling

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,29 @@
 #!/usr/bin/node
 const request = require('request');
 
-function fetchEndpoint(url) {
+function fetchEndpoint (url) {
   return new Promise(function (resolve, reject) {
-    request(url, {json: true}, (err, res, body) => {
+    request(url, { json: true }, (err, res, body) => {
       if (err) {
         reject(err);
-      } else if (res.statusCode !==  200){
-          reject(new Error(`Request Failed. status code: ${res.statusCode}`));
+      } else if (res.statusCode !== 200) {
+        reject(new Error(`Request Failed. status code: ${res.statusCode}`));
       } else {
-        resolve (body)
+        resolve(body);
       }
     });
-    });
+  });
 }
 
-const URL = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`
+const URL = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 
 fetchEndpoint(URL).then((data) => {
-  const peopleLinks = data.characters
+  const peopleLinks = data.characters;
   return Promise.all(peopleLinks.map(url => fetchEndpoint(url)));
-}).then ((peopleData)=>{
+}).then((peopleData) => {
   peopleData.forEach(person => {
     console.log(person.name);
   });
-}). catch((error)=> {
-  console.error('Error', error)
+}).catch((error) => {
+  console.error('Error', error);
 });
-
-
-
-
