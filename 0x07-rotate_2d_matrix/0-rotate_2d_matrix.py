3796855e1201d11e0b329856997946f92546d6e6
Y4h14 <yahyasadig2013@gmail.com>
2024-07-21 20:58:23 +0300

    initial commit

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def rotate_2d_matrix(matrix):
+    """rotates a 2D matrix"""
+
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            temp =  matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+    for i in range(len(matrix)):
+        matrix[i].reverse()
